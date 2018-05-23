b3e5a9ed0f594f56eb64fa1a192a460f070e8e2a
Unknown <alice.burette@etu.umontpellier.fr>
2018-05-23 22:30:28 +0200

    project final

--- burette/alice/player/HardcoreAI.java
+++ burette/alice/player/HardcoreAI.java
@@ -1,6 +1,7 @@
 package burette.alice.player;
 
-public class HardcoreAI extends AI {
+public class UltraHardcoreAI extends AI {
+
     int[] lastRandomHit = null;
     String lastshotResult;
 
@@ -16,68 +17,62 @@ public class HardcoreAI extends AI {
             // dernier tir --> shots.get(shots.size()-1)
             if (lastshotResult == "Coulé !") {
                 lastRandomHit = null; // on réinitialise lastRandomHit pour chercher un nouveau bateau en random
-            }
-            else {
-                if (shots.get(shots.size()-1)[0] == lastRandomHit[0]) {
-                    if (shots.get(shots.size()-1)[1] >= lastRandomHit[1]) {
+            } else {
+                if (shots.get(shots.size() - 1)[0] == lastRandomHit[0]) {
+                    if (shots.get(shots.size() - 1)[1] >= lastRandomHit[1]) {
                         // zone 1
-                        if (lastshotResult == "Touché !" && (shots.get(shots.size()-1)[1] + 1) < 10) {
+                        if (lastshotResult == "Touché !" && (shots.get(shots.size() - 1)[1] + 1) < 10) {
                             coord[0] = lastRandomHit[0];
-                            coord[1] = shots.get(shots.size()-1)[1] + 1;
-                        }
-                        else {
-                            if (lastRandomHit[1] > 0){
+                            coord[1] = shots.get(shots.size() - 1)[1] + 1;
+                        } else {
+                            if (lastRandomHit[1] > 0) {
                                 // le dernier tir est râté
                                 coord[0] = lastRandomHit[0];
                                 coord[1] = lastRandomHit[1] - 1;
 
                             }
                         }
-                    }
-                    else {
+                    } else {
                         // zone 3
-                        if (lastshotResult == "Touché !" && (shots.get(shots.size()-1)[1]) > 0) {
+                        if (lastshotResult == "Touché !" && (shots.get(shots.size() - 1)[1]) > 0) {
                             coord[0] = lastRandomHit[0];
-                            coord[1] = shots.get(shots.size()-1)[1] - 1;
-                        }
-                        else { // le dernier tir est râté
+                            coord[1] = shots.get(shots.size() - 1)[1] - 1;
+                        } else { // le dernier tir est râté
                             if ((lastRandomHit[0] + 1) < 10) {
                                 coord[0] = lastRandomHit[0] + 1;
                                 coord[1] = lastRandomHit[1];
                             }
                         }
                     }
-                }
-                else {
-                    if (shots.get(shots.size()-1)[0] > lastRandomHit[0]) {
+                } else {
+                    if (shots.get(shots.size() - 1)[0] > lastRandomHit[0]) {
                         // zone 2
-                        if (lastshotResult == "Touché !" && (shots.get(shots.size()-1)[0] + 1)<10) {
-                            coord[0] = shots.get(shots.size()-1)[0] + 1;
+                        if (lastshotResult == "Touché !" && (shots.get(shots.size() - 1)[0] + 1) < 10) {
+                            coord[0] = shots.get(shots.size() - 1)[0] + 1;
                             coord[1] = lastRandomHit[1];
-                        }
-                        else { // le dernier tir est râté
+                        } else { // le dernier tir est râté
                             if (lastRandomHit[0] > 0) {
                                 coord[0] = lastRandomHit[0] - 1;
                                 coord[1] = lastRandomHit[1];
                             }
                         }
-                    }
-                    else {
+                    } else {
                         // zone 4
-                        if (shots.get(shots.size()-1)[0] > 0) {
+                        if (shots.get(shots.size() - 1)[0] > 0) {
                             coord[0] = shots.get(shots.size() - 1)[0] - 1;
                             coord[1] = lastRandomHit[1];
-                        }
                         }
                     }
                 }
             }
+        }
         if (lastRandomHit == null) {
             boolean firstShot = true;
             while (firstShot) {
                 firstShot = false;
-                coord[0] = (int)(Math.random()*9.99);
-                coord[1] = (int)(Math.random()*9.99);
+                int parity = (int) (Math.random() * 1.99);
+                coord[0] = ((int) (Math.random() * 4.99)) * 2 + parity;
+                coord[1] = ((int) (Math.random() * 4.99)) * 2 + parity;
                 for (int loop = 0; loop < this.shots.size(); loop++) {
                     if (coord[1] == this.shots.get(loop)[1] && coord[0] == this.shots.get(loop)[0]) {
                         firstShot = true;
@@ -98,5 +93,7 @@ public class HardcoreAI extends AI {
     public void reset() {
         super.reset();
         this.lastRandomHit = null;
+
     }
 }
+
