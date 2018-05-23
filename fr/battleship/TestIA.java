f2ed3d50a797a2377181e394c48b1dabe77c85e0
Unknown <alice.burette@etu.umontpellier.fr>
2018-05-23 13:11:20 +0200

    fileWriter added

--- fr/battleship/TestIA.java
+++ fr/battleship/TestIA.java
@@ -3,8 +3,20 @@ package fr.battleship;
 import burette.alice.*;
 import burette.alice.player.*;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TestIA {
     public static void main(String[] args) {
+
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("ai_proof.csv");
+            writer.write("AI Name; score; AI Name2; score2\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         AI eia = new EasyAI();
         AI mia = new MediumAI();
@@ -18,6 +30,7 @@ public class TestIA {
         int s1 = 0;
         int s2 = 0;
 
+
         for (int i = 0; i<100; i++) {
             IPlay res = game1.autoGame();
             if (res == eia) {
@@ -25,6 +38,11 @@ public class TestIA {
             }
             if (res == mia)
                 s2++;
+        }
+        try {
+            writer.write("AI Easy;"+s1+"; AI Medium;"+s2+"\n");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         System.out.println("easy VS medium");
         System.out.println("score IA easy : "+s1);
@@ -42,6 +60,11 @@ public class TestIA {
             if (res == uia)
                 s2++;
         }
+        try {
+            writer.write("AI Easy;"+s1+"; AI Hardcore;"+s2+"\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("easy VS hardcore");
         System.out.println("score IA easy : "+s1);
         System.out.println("score IA hardcore : "+s2);
@@ -58,10 +81,20 @@ public class TestIA {
             if (res == uia)
                 s2++;
         }
+        try {
+            writer.write("AI Medium;"+s1+"; AI Hardcore;"+s2+"\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("medium VS hardcore");
         System.out.println("score IA medium : "+s1);
         System.out.println("score IA hardcore : "+s2);
         System.out.println(" ");
 
+        try {
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
